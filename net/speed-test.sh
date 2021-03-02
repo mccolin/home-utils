2d14c2dd9ad392f5315552b0509cb07d5fd0b61d
Colin McCloskey <mccolin@users.noreply.github.com>
2021-03-02 12:18:15 -0500

    Forward CLI args to speedtest script

--- net/speed-test.sh
+++ net/speed-test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
+curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python - $@
